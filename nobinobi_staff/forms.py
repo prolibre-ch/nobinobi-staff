720302c3f4ef35b31bbd5dae6c91a9e5f5220f6d
Florian Alu <alu@prolibre.com>
2020-02-27 11:40:58 +0100

    fix double adresse verification

--- nobinobi_staff/forms.py
+++ nobinobi_staff/forms.py
@@ -17,27 +17,27 @@ class AbsenceAdminForm(forms.ModelForm):
 
         """Constructor for AbsenceAdminForm"""
 
-    # def clean(self):
-    #     cleaned_data = super().clean()
-    #     # get value
-    #     staff = cleaned_data.get('staff')
-    #     start_date = cleaned_data.get("start_date")
-    #     end_date = cleaned_data.get("end_date")
-    #     # create a range from form
-    #     form_absence_range = DateTimeRange(start_date, end_date)
-    #     # get absences
-    #     absences = Absence.objects.filter(
-    #         start_date__lte=end_date,
-    #         end_date__gte=start_date,
-    #         staff_id=staff.id
-    #     )
-    #     for absence in absences:
-    #         # create a range absence
-    #         absence_range = DateTimeRange(absence.start_date, absence.end_date)
-    #         # if form range is intersection with absence range raise error
-    #         if form_absence_range.is_intersection(absence_range):
-    #             raise forms.ValidationError(_('An absence already exists on these dates.'), code='invalid')
-    #     return cleaned_data
+    def clean(self):
+        cleaned_data = super().clean()
+        # get value
+        staff = cleaned_data.get('staff')
+        start_date = cleaned_data.get("start_date")
+        end_date = cleaned_data.get("end_date")
+        # create a range from form
+        form_absence_range = DateTimeRange(start_date, end_date)
+        # get absences
+        absences = Absence.objects.filter(
+            start_date__lte=end_date,
+            end_date__gte=start_date,
+            staff_id=staff.id
+        ).exclude(pk=self.instance.pk)
+        for absence in absences:
+            # create a range absence
+            absence_range = DateTimeRange(absence.start_date, absence.end_date)
+            # if form range is intersection with absence range raise error
+            if form_absence_range.is_intersection(absence_range):
+                raise forms.ValidationError(_('An absence already exists on these dates.'), code='invalid')
+        return cleaned_data
 
     def __init__(self, *args, **kwargs):
         try:
