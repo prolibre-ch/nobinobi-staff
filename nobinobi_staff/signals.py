49863c5df3792e19520b47968ad69a5f62b44959
Florian Alu <alu@prolibre.com>
2020-03-18 11:14:17 +0100

    Fix error on signal if rt not exist

--- nobinobi_staff/signals.py
+++ nobinobi_staff/signals.py
@@ -16,19 +16,19 @@ def update_training_for_staff(sender, instance, created, raw, using, **kwargs):
     now = timezone.localdate()
     rt = RightTraining.objects.first()
     # +1 for accept 12 in range
-    if rt.start_month in range(9, 12 + 1):
-        if now.month in range(9, 13):
-            start_date = arrow.get(datetime.date(timezone.localdate().year, rt.start_month, rt.start_day))
-        else:
-            start_date = arrow.get(datetime.date(timezone.localdate().year - 1, rt.start_month, rt.start_day))
-        end_date = start_date.shift(years=1, days=-1)
-    else:
-        if now.month in range(9, 13):
-            start_date = arrow.get(datetime.datetime(timezone.localdate().year, rt.start_month, rt.start_day))
-        else:
-            start_date = arrow.get(datetime.datetime(timezone.localdate().year - 1, rt.start_month, rt.start_day))
-        end_date = start_date.shift(years=1, days=-1)
     if rt:
+        if rt.start_month in range(9, 12 + 1):
+            if now.month in range(9, 13):
+                start_date = arrow.get(datetime.date(timezone.localdate().year, rt.start_month, rt.start_day))
+            else:
+                start_date = arrow.get(datetime.date(timezone.localdate().year - 1, rt.start_month, rt.start_day))
+            end_date = start_date.shift(years=1, days=-1)
+        else:
+            if now.month in range(9, 13):
+                start_date = arrow.get(datetime.datetime(timezone.localdate().year, rt.start_month, rt.start_day))
+            else:
+                start_date = arrow.get(datetime.datetime(timezone.localdate().year - 1, rt.start_month, rt.start_day))
+            end_date = start_date.shift(years=1, days=-1)
         training, created = Training.objects.get_or_create(
             staff=instance,
             start_date=start_date.date(),
